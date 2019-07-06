9c402ff51ea3b8fa8b8b1a2c8138cd4247380495
Witek <witold.szrama@gmail.com>
2019-07-06 17:37:52 +0200

    update snake bug

--- snake/game.js
+++ snake/game.js
@@ -1,27 +1,61 @@
 let t;
+let old_key;
 ball();
 pinki = document.querySelector('.ran');
+
 window.addEventListener('keydown', e=>{
 
   clearInterval(t)
 
   pinki = document.querySelector('.ran');
+  
+  if(!(e.key=='ArrowUp' || e.key=='ArrowDown' || e.key=='ArrowLeft' || e.key=='ArrowRight')){
+
+    switch(old_key){
+
+      case 'ArrowUp':
+        old_key=e.key
+        t=setInterval(up,time);
+        break;
+
+      case 'ArrowDown':
+        old_key=e.key
+        t=setInterval(down,time)
+        break;
+
+      case 'ArrowLeft':
+        old_key=e.key
+      t=setInterval(left,time);
+        break;
+
+      case 'ArrowRight':
+        old_key=e.key
+      t=setInterval(right,time);
+        break;
+      }
+  }
+
+
 
   switch(e.key){
 
     case 'ArrowUp':
+      old_key=e.key
       t=setInterval(up,time);
       break;
 
     case 'ArrowDown':
+      old_key=e.key
       t=setInterval(down,time)
       break;
 
     case 'ArrowLeft':
+      old_key=e.key
     t=setInterval(left,time);
       break;
 
     case 'ArrowRight':
+      old_key=e.key
     t=setInterval(right,time);
       break;
 
